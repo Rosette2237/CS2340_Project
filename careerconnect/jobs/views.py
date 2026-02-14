3ecc896b68d85ca44f93736cef1e8096cdd4c831
Mehul Patil <mp.mehulpatil@gmail.com>
2026-02-14 05:35:33 -0500

    Recc System + UI Changes

--- careerconnect/jobs/views.py
+++ careerconnect/jobs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Job
+from .utils import calculate_match
 from django.db.models import Q
 
 def job_list(request):
-    jobs = Job.objects.all().order_by('-posted_at')
+    jobs = Job.objects.all()
 
     title_query = request.GET.get('title')
     location_query = request.GET.get('location')
@@ -12,18 +13,27 @@ def job_list(request):
 
     if title_query:
         jobs = jobs.filter(title__icontains=title_query)
-    
+
     if location_query:
         jobs = jobs.filter(location__icontains=location_query)
-        
+
     if job_type_query:
         jobs = jobs.filter(job_type=job_type_query)
 
     if visa_query == 'on':
         jobs = jobs.filter(visa_sponsorship=True)
 
+    if request.user.is_authenticated:
+        try:
+            profile = request.user.profile
+            for job in jobs:
+                job.match_percent = calculate_match(job.skills_required, profile.skills)
+            jobs = sorted(jobs, key=lambda x: (-x.match_percent, -x.posted_at.timestamp()))
+        except:
+            pass
+
     context = {
         'jobs': jobs,
-        'values': request.GET 
+        'values': request.GET
     }
     return render(request, 'jobs/job_list.html', context)
